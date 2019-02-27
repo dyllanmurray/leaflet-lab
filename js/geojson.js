1158f8e17c42a756ad484603d6ceb86f5f6bca01
Dyllan Murray <dmurray@innovateteam.com>
2019-02-27 13:23:16 -0700

    added css to match Outside Mag
    added css to match Outside Mag

--- js/geojson.js
+++ js/geojson.js
@@ -1,38 +1,55 @@
 function createMap(){
     //create the map
-    var OSMLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-    });
-    
-     //add black and white base tilelayer
-    var blackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-    
-    //add stamen map
-    var topo = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
-        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> — Map data © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    //dark matter basemap
+    var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
         subdomains: 'abcd',
-        minZoom: 0,
-        maxZoom: 20,
-        ext: 'png'
+        maxZoom: 19
+    });
+    
+     //addesri imagery tilelayer
+     var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    });
+    
+    //add stamen water color map
+    var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 1,
+	maxZoom: 16,
+	ext: 'jpg'
     });
 
+    //add esri_ world physical basemap
+    var Esri_WorldPhysical = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Physical_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: US National Park Service',
+	maxZoom: 8
+    });
+    //Esri world topo
+    var Esri_WorldTopoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+    });
+
+
     var baseMaps = {
-        "OpenStreetMap": OSMLayer,
-        "Black and White": blackAndWhite,
-        "Stamen Tiles": topo
-        
+        "Dark Matter": CartoDB_DarkMatter,
+        "Imagery": Esri_WorldImagery,
+        "Stamen Water Color": Stamen_Watercolor,
+        "Physical": Esri_WorldPhysical,
+        "Topo": Esri_WorldTopoMap
     };
+
     var map = L.map('map', {
         center:  [37.117, -103.333],
         zoom: 5,
-        layers: [OSMLayer]
+        layers: [Esri_WorldTopoMap]
     });
 
-    //add OSM base tilelayer
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    //add esri basemao tilelayer
+    
+    var Esri_WorldTopoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
     }).addTo(map);
 
         let cities =L.layerGroup()
@@ -59,46 +76,24 @@ function createMap(){
 
 // function to retrieve the data and place it on the map
 // This snippet loops through the geojson and prepares a popup for each feature/////
-function onEachFeature(feature, layer) {
-    //no property named popupContent; instead, create html string with all properties
-    var popupContent = "";
-    if (feature.properties) {
-        //loop to add feature property names and values to html string
-        for (var property in feature.properties){
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-        }
-        layer.bindPopup(popupContent);
-    };
-};
-function createPropSymbols(data, map){
-    //create marker options
-    var geojsonMarkerOptions = {
-        radius: 8,
-        fillColor: "#ff7800",
-        color: "#000",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
-    };
-     //Step 4: Determine which attribute to visualize with proportional symbols
-    var attribute = "Pop_2010";
-
+// function onEachFeature(feature, layer) {
+//     //no property named popupContent; instead, create html string with all properties
+//     var popupContent = "";
+//     if (feature.properties) {
+//         //loop to add feature property names and values to html string
+//         for (var property in feature.properties){
+//             popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+//         }
+//         layer.bindPopup(popupContent);
+//     };
+// };
+//Add circle markers for point features to the map
+function createPropSymbols(data, map, attributes){
     //create a Leaflet GeoJSON layer and add it to the map
     L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
-            //Step 5: For each feature, determine its value for the selected attribute
-            var attValue = Number(feature.properties[attribute]);
-
-            //Step 6: Give each feature's circle marker a radius based on its attribute value
-            geojsonMarkerOptions.radius = calcPropRad(attValue);
-
-            //examine the attribute value to check that it is correct
-            console.log(feature.properties, attValue);
-
-            //create circle markers
-            return L.circleMarker(latlng, geojsonMarkerOptions);
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
         }
-
     }).addTo(map);
 };
 
@@ -110,13 +105,14 @@ function getData(map){
         success: function(response){
             //call function to create proportional symbols
             createPropSymbols(response, map);
+            createSequenceControls(map);
         }
     });
 };
 
 function calcPropRad(attValue) {
     //scale factor to adjust symbol size evenly
-    var scaleFactor = .003;
+    var scaleFactor = .002;
     //area based on attribute value and scale factor
     var area = attValue * scaleFactor;
     //radius calculated based on area
@@ -126,5 +122,75 @@ function calcPropRad(attValue) {
 
 };
 
+function pointToLayer(feature, latlng){
+    //Determine which attribute to visualize with proportional symbols
+    var attribute = "Pop_2015";
+
+    //create marker options
+    var options = {
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRad(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+    //build popup content string
+    var popupContent = "<p><b>City:</b> " + feature.properties.City + "</p>";
+
+    //add formatted attribute to popup content string
+    var year = attribute.split("_")[1];
+    popupContent += "<p><b>Population in " + year + ":</b> " + feature.properties[attribute] + "</p>";
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent);
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+function createSequenceControls(map){
+    var SequenceControl = L.Control.extend({
+        options: {
+            position: 'bottomleft'
+        },
+
+       onAdd: function (map) {
+            // create the control container with a particular class name
+            var container = L.DomUtil.create('div', 'sequence-control-container');
+
+            //create range input element (slider)
+            $(container).append('<input class="range-slider" type="range">');
+
+            //add skip buttons
+            $(container).append('<button class="skip" id="reverse" title="Previous Year">Previous</button>');
+            $(container).append('<button class="skip" id="forward" title="Next Year">Next</button>');
+
+            $(container).on('mousedown dblclick', function(e){
+				L.DomEvent.stopPropagation(e);
+            });
+
+            return container;
+        }
+    });
+
+    map.addControl(new SequenceControl());
+
+    //set slider attributes
+    $('.range-slider').attr({
+        max: 6,
+        min: 0,
+        value: 0,
+        step: 1
+    });
+};
 
 $(document).ready(createMap);
